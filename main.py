62e56220c423e2b18168b7e5e5392cf3ae53b9c6
Waelahmed99 <waelahmed6599@gmail.com>
2021-11-17 09:49:40 +0200

    refactoring main entry

---
+++ main.py
@@ -0,0 +1,59 @@
+import json
+import os
+from requests.api import head
+from helpers.trainees import getTrainees
+from helpers.groups import getGroups
+from services.jsonbin import generateBin
+from services.rebrandly import shortenLink
+from helpers.colors import bcolors
+from dotenv import load_dotenv
+load_dotenv()
+
+def saveToFile(fileName, data):
+    with open(f'{fileName}', 'w+') as file:
+        file.write(data)
+
+
+def getBoardData():
+    return {
+        'groups': getGroups(),
+        'trainees': getTrainees(),
+    }
+
+def getFullBoardUrl(json_url):
+    board_url = "https://boardy.cf/?configs={}".format(json_url)
+    print(f"{bcolors.OKYELLOW}\nBoard successfully generated with the following link:")
+    print(f"{bcolors.OKBLUE}{board_url}\n{bcolors.ENDC}")
+    return board_url
+
+
+def main():
+        
+    print(f"{bcolors.HEADER}Welcome to Board link generator!\n{bcolors.ENDC}")
+    
+    # creating dictionary of required board's data 
+    board =  getBoardData()
+    
+    # saving board data as json in board.json.
+    saveToFile('board.json', json.dumps(board))
+    
+    print(f"{bcolors.OKYELLOW}\nJson data successfully created in board.json{bcolors.ENDC}")
+    
+    # generating bin for board's json
+    json_bin_url = generateBin(board)
+    
+    # getting the full url for boardy.cf using configs queryString.
+    full_board_url = getFullBoardUrl(json_bin_url)
+    
+    # saving jsonbin link and full board url in links.txt.
+    saveToFile('links.txt', f"jsonbin url: {json_bin_url}\nFull board url: {full_board_url}\n")
+    
+    # generating shorturl to the full board url
+    shorten_url = shortenLink(full_board_url)
+    
+    # saving the shorten url in links.txt.
+    saveToFile('links.txt', f"Shorten url: {shorten_url}\n")
+
+         
+if __name__ == "__main__":
+    main()
